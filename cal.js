c75c3ab854e738bdbd8385c802a2dd4f0cbb09ab
codebybiswajit <136327311+codebybiswajit@users.noreply.github.com>
2024-04-15 23:04:02 +0530

    Update cal.js

--- cal.js
+++ cal.js
@@ -8,7 +8,11 @@ Array.from(buttons).forEach((element) => {
 
             inputstring = eval(document.querySelector('.inputtext').value)
             document.querySelector('.inputtext').value = inputstring 
-        }else if (element.value == "AC") {
+        }else if (element.value == "del") {
+            inputstring = inputstring.substring(0,inputstring.length-1)
+            document.querySelector('.inputtext').value = inputstring
+        }
+        else if (element.value == "AC") {
             inputstring =''
             document.querySelector('.inputtext').value = inputstring
         }
